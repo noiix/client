8abc4dec3d225c5468e7821fd9955a5fe9730e5d
Fridabug <friederikespring@posteo.de>
2022-09-27 14:12:05 +0200

    .

--- src/components/UI/alerts/Alert.jsx
+++ src/components/UI/alerts/Alert.jsx
@@ -1,4 +1,18 @@
+import { useContext } from "react"
+import { useEffect } from "react"
+import { useState } from "react"
+import UserContext from "../../../contexts/UserContext"
+
 const SuccessAlert = ({title, type}) => {
+    const {notification} = useContext(UserContext)
+    const [blendIn, setBlendIn] = useState(false)
+
+    useEffect(()=> {
+        const timer = setTimeout(() => {
+
+        }, 10000)
+        return () => clearTimeout(timer)
+    }, [notification])
 
     return (
         <div className={`alert ${type}`}>

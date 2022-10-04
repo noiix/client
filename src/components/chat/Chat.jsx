78029479e5f2b469d4e0fd207d7da11c7ce2c79b
siggi <sigurdurgur@gamil.com>
2022-10-04 10:15:11 +0200

    pullcommit666

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,26 +3,26 @@ import ChatContext from '../../contexts/ChatContext'
 
 function Chat() {
 
-  const send = (e) => {
-    if(e.key === "Enter"){
-      if() {
+  // const send = (e) => {
+  //   if(e.key === "Enter"){
+  //     if() {
 
-      } else {
+  //     } else {
 
-      }
-    }
-  }
+  //     }
+  //   }
+  // }
 
-  return (
-    <div className='chat-container'>
-      <div className='chat'>
-        <div id='messages'></div>
-        <div>
-          <input type='text' onKeyUp={(e) => send(e)} id='message' placeholder='your message...'/>
-        </div>
-      </div>
-    </div>
-  )
+  // return (
+  //   <div className='chat-container'>
+  //     <div className='chat'>
+  //       <div id='messages'></div>
+  //       <div>
+  //         <input type='text' onKeyUp={(e) => send(e)} id='message' placeholder='your message...'/>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default Chat

4efa95e02163b43df4e03f1daeaa51e40e18ecb2
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-10-04 16:41:45 +0200

    .

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -90,7 +90,7 @@ export const UserProvider = ({ children }) => {
   };
 
   const logout = () => {
-    axios
+    API
       .get(`${baseUrl}/user/logout`)
       .then((response) => {
         // localStorage.clear();

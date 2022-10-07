f059bd694dad9de0c39c8ce84ab09bc40a15a6be
Fridabug <friederikespring@posteo.de>
2022-10-07 14:24:10 +0200

    .

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -16,14 +16,8 @@ const Register = () => {
   console.log(currentUser);
 
   return (
-<<<<<<< HEAD
     <>
       <form className="signupForm" onSubmit={ createAccount }>
-=======
-    <div>
-
-      <form className="signupForm">
->>>>>>> 7b46b66e85d5a76e4ff143798acda87ae842c68c
         <input
           type="text"
           name="username"
@@ -45,13 +39,7 @@ const Register = () => {
           placeholder="password"
           onChange={ inputHandler }
           required
-<<<<<<< HEAD
         />
-=======
-        />{ " " }
-        <br />
-        <Button className="btn" type="submit" value="submit" onClick={createAccount} name="sign up"/>
->>>>>>> 7b46b66e85d5a76e4ff143798acda87ae842c68c
       </form>
       <div>
         <Button className="btn" type="submit" value="submit" name="sign up"/>

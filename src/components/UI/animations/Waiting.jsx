360ee1cec277d2f1f0c73c97d419a4fc5e242de5
siggi <sigurdurgur@gamil.com>
2022-10-27 11:20:14 +0200

    animation fix

--- src/components/UI/animations/Waiting.jsx
+++ src/components/UI/animations/Waiting.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {ImSpinner2} from 'react-icons/im';
+import { ImSpinner2 } from 'react-icons/im';
 import './waiting.styling.scss';
 
 function Waiting() {
   return (
-    <div className='waiting-animation'>
-        <ImSpinner2/>    
+    <div id='waiting-animation'>
+      <ImSpinner2 />
     </div>
   )
 }

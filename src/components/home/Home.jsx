32584b44488c0872c703de4b1e0a096af3a5b49c
Fridabug <friederikespring@posteo.de>
2022-11-10 15:23:54 +0100

    animation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,10 @@ import UserContext from "../../contexts/UserContext";
 import CardList from "../UI/CardList/CardList";
 import Modal from "../UI/modal/Modal";
 import Button from "../UI/button/Button";
-import useLocalStorage from 'use-local-storage'
+import './animation/home-animation.styling.scss'
+import {ReactComponent as DrumSet} from './animation/drum_set.svg';
+import {ReactComponent as Ukulele} from './animation/ukulele.svg';
+import {ReactComponent as Flute} from './animation/flute.svg';
 
 
 function Home() {
@@ -23,7 +26,19 @@ function Home() {
       </div>
         
 
-        { (Object.keys(currentUser).length > 0) && <CardList/> }
+        { (Object.keys(currentUser).length > 0 && users.length > 0) ? <CardList/> : 
+          <>
+            <div className="introduction">
+              <h3>Welcome to Nöix</h3>
+              <p>to find other musicians nearby you need to set your genres in your profile.</p>
+            </div>        
+            <div className="home-animation">  
+              <Ukulele className="ukulele-svg"/>
+              <Flute className="flute-svg"/>
+              <DrumSet className="drum-set-svg"/>
+            </div>
+          </>
+        }
 
         { (Object.keys(currentUser).length === 0 && !toggleBtn) &&
           <div className="login-btn-container">
